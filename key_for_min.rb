32916f8c2b9621ea5ba4a5b4a88a5d6d3946d3cf
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-06 11:54:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   end
-  value = 1000000
+  value_min = 1000000
+  key_min = "IDK"
   name_hash.each  {|key, value|
     if value < value_min
       key_min = key
